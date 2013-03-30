0e74167401043328ba5cc1d14dbdda8ae1faf6d5
J.D. Hollis <jd@densityofspace.com>
2013-03-30 13:08:54 -0400

    Add forecast function

--- src/forecast_clojure/core.clj
+++ src/forecast_clojure/core.clj
@@ -3,3 +3,13 @@
         [environ.core])
   (:require [cheshire.core :as json]
             [clj-http.client :as client]))
+
+(defn forecast
+  "Retrieve the forecast for a given latitude and longitude"
+  [lat lon & {:keys [params time]}]
+  (let [base-url "https://api.forecast.io/forecast"
+        api-key (env :forecast-key)
+        url (join "/" [base-url api-key (join "," (filter #(not-empty %) [lat lon time]))])
+        response (client/get url {:query-params params :throw-exceptions false})]
+    (cond (= 200 (:status response))
+          (json/parse-string (:body response) true))))
