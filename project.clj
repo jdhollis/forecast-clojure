2205c55da13d9690621b1ecf5f8e9150bc3123c7
J.D. Hollis <jd@densityofspace.com>
2013-03-30 18:12:01 -0400

    Convert arguments to strings before trying to filter

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject forecast-clojure "1.0.2"
+(defproject forecast-clojure "1.0.3"
   :description "Clojure library for retrieving data from the Forecast API"
   :url "https://github.com/jdhollis/forecast-clojure"
   :license {:name "Eclipse Public License"
