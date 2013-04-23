27b5f04f44152533fc238d9f4379255b325ef7d7
Dan Hammer <dan.s.hammer@gmail.com>
2013-04-23 14:01:58 -0700

    minor suggestions

--- src/forecast_clojure/core.clj
+++ src/forecast_clojure/core.clj
@@ -1,15 +1,16 @@
 (ns forecast-clojure.core
-  (:use [clojure.string :only [join]]
-        [environ.core])
+  (:use environ.core
+        [clojure.string :only (join)])
   (:require [cheshire.core :as json]
             [clj-http.client :as client]))
 
 (defn forecast
   "Retrieve the forecast for a given latitude and longitude"
-  [lat lon & {:keys [params time]}]
+  [lat lon & {:keys [params time] :or [params nil time nil]}]
   (let [base-url "https://api.forecast.io/forecast"
         api-key (env :forecast-key)
-        url (join "/" [base-url api-key (join "," (filter #(not-empty %) (map str [lat lon time])))])
+        query (apply str (interpose "," [lat lon time]))
+        url (join "/" [base-url api-key query])
         response (client/get url {:query-params params :throw-exceptions false})]
     (cond (= 200 (:status response))
           (json/parse-string (:body response) true))))
